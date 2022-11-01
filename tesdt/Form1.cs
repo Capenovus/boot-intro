0a9eed181ae794a8dd20af8f929b18cb0471899e
Boring <magnus_g@outlook.de>
2022-11-01 12:33:06 +0100

    Add project files.

---
+++ tesdt/Form1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.DirectX.AudioVideoPlayback;
+using System;
+using System.Windows.Forms;
+
+namespace tesdt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+            Video v = new Video(@"C:\Users\boring\source\misc\ps1\video.mp4")
+            {
+                Owner = this,
+                Size = Screen.PrimaryScreen.Bounds.Size
+            };
+            v.Ending += (s, ev) => { Close(); };
+            v.Play();
+        }
+    }
+}
---
+++ tesdt/LogonUI.bat
@@ -0,0 +1,13 @@
+@echo off
+find "0" C:\ps1_state.txt && (
+goto :yay
+) || (
+goto :end
+)
+:yay
+timeout /t 3
+echo 1 > C:\ps1_state.txt
+C:\Users\boring\source\repos\tesdt\tesdt\bin\x86\Debug\tesdt.exe
+:end
+LogonUI_o.exe %*
+taskkill /IM LockApp.exe /F
