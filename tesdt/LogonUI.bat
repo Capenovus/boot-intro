1851fb01c2d7fa81bd08125a866120da9d1b303b
Capenovus <57587298+Capenovus@users.noreply.github.com>
2023-02-28 23:53:29 +0100

    Update LogonUI.bat

--- tesdt/LogonUI.bat
+++ tesdt/LogonUI.bat
@@ -8,7 +8,7 @@ goto :end
 :yay
 timeout /t 3
 echo 1 > C:\ps1_state.txt
-C:\Users\...\source\repos\tesdt\tesdt\bin\x86\Debug\tesdt.exe
+C:\Users\...\tesdt.exe
 :end
 LogonUI_o.exe %*
 taskkill /IM LockApp.exe /F
