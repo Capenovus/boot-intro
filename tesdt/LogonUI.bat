a81343a4845672ebe526ecee44a409c82367791c
Capenovus <57587298+Capenovus@users.noreply.github.com>
2023-06-24 15:10:59 +0200

    Update LogonUI.bat

--- tesdt/LogonUI.bat
+++ tesdt/LogonUI.bat
@@ -1,4 +1,4 @@
-Rem AutoExecute on Boot, Compile as LogonUI.exe and replace in Sys32 (also rename the original to LogonUI_o.exe)
+:: Compile as LogonUI.exe and replace in Sys32 (also rename the original to LogonUI_o.exe)
 @echo off
 find "0" C:\ps1_state.txt && (
 goto :yay
@@ -6,9 +6,12 @@ goto :yay
 goto :end
 )
 :yay
+:: Timeout specific to my multi-monitor configuration, you may or may not need it
 timeout /t 3
 echo 1 > C:\ps1_state.txt
 C:\Users\...\tesdt.exe
 :end
 LogonUI_o.exe %*
+
+:: Fix Visual Glitch
 taskkill /IM LockApp.exe /F
